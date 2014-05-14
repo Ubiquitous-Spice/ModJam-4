dbd2f503c8e8d5e9c1572040321bc1eb264a3c12
Royalixor <royalixor@gmail.com>
2014-05-15 00:37:30 +0100

    Basic mod classes and method calls

---
+++ src/main/java/com/github/ubiquitousspice/modularmachines/core/proxy/ClientProxy.java
@@ -0,0 +1,7 @@
+package com.github.ubiquitousspice.modularmachines.core.proxy;
+
+/**
+ * @author Royalixor.
+ */
+public class ClientProxy extends CommonProxy {
+}
---
+++ src/main/java/com/github/ubiquitousspice/modularmachines/util/ModProps.java
@@ -0,0 +1,14 @@
+package com.github.ubiquitousspice.modularmachines.util;
+
+/**
+ * @author Royalixor.
+ */
+public class ModProps {
+
+    public static final String ID = "MM";
+    public static final String NAME = "ModularMachines";
+    public static final String VERSION = "@VERSION@";
+
+    public static final String COMMON_PROXY = "com.github.ubiquitousspice.modularmachines.core.proxy.CommonProxy";
+    public static final String CLIENT_PROXY = "com.github.ubiquitousspice.modularmachines.core.proxy.ClientProxy";
+}
