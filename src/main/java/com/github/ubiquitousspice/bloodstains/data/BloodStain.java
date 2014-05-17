43c0652bc92c14caf40fa3b7f9643b3269c3efd0
AbrarSyed <sacabrarsyed@gmail.com>
2014-05-17 15:01:55 -0500

    added persistance methods

--- src/main/java/com/github/ubiquitousspice/bloodstains/data/BloodStain.java
+++ src/main/java/com/github/ubiquitousspice/bloodstains/data/BloodStain.java
@@ -1,11 +1,15 @@
 package com.github.ubiquitousspice.bloodstains.data;
 
-import com.google.common.collect.ImmutableList;
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
 
-import java.util.LinkedList;
-import java.util.List;
+import com.google.common.collect.ImmutableList;
 
 @EqualsAndHashCode
 @ToString
@@ -14,25 +18,60 @@ public class BloodStain
 	public final int dimId;
 	public final double x, y, z;
 	public final String username;
-	public final ImmutableList<PlayerState> states;
+	public final List<PlayerState> states;
 
-	public BloodStain(PlayerStateContainer playerStateContainer)
+	public BloodStain(PlayerStateContainer container)
 	{
-		this.username = playerStateContainer.username;
-		List<PlayerState> statesList = new LinkedList<PlayerState>();
-		for (PlayerState p : playerStateContainer.states)
-		{
-			if (p != null)
-			{
-				statesList.add(p);
-			}
-
-		}
-		states = ImmutableList.copyOf(statesList);
+		this.username = container.username;
+		states = ImmutableList.copyOf(container.states);
 		PlayerState firstState = states.get(0);
-		dimId = firstState.dimension;
-		x = firstState.x;
-		y = firstState.y;
-		z = firstState.z;
+		dimId = firstState.getDimension();
+		x = firstState.getX();
+		y = firstState.getY();
+		z = firstState.getZ();
 	}
+	
+    private BloodStain(String username, int dim, double x, double y, double z, List<PlayerState> states)
+    {
+        this.username = username;
+        this.dimId = dim;
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        
+        this.states = states;
+    }
+
+    public void writeTo(DataOutput output) throws IOException
+    {
+        // write ID
+        output.writeUTF(username);
+        
+        // write location
+        output.writeInt(dimId);
+        output.writeDouble(x);
+        output.writeDouble(y);
+        output.writeDouble(z);
+        
+        // states
+        output.writeInt(states.size());
+        for (PlayerState state : states)
+            state.writeTo(output);
+    }
+
+    public static BloodStain readFrom(DataInput input) throws IOException
+    {
+        String username = input.readUTF();
+        int dim = input.readInt();
+        double x = input.readDouble();
+        double y = input.readDouble();
+        double z = input.readDouble();
+        
+        int size = input.readInt();
+        ArrayList<PlayerState> states = new ArrayList<PlayerState>(input.readInt());
+        for (int i = 0; i < size; i++)
+            states.add(PlayerState.readFrom(input));
+        
+        return new BloodStain(username, dim, x, y, z, states);
+    }
 }
--- src/main/java/com/github/ubiquitousspice/bloodstains/data/PlayerState.java
+++ src/main/java/com/github/ubiquitousspice/bloodstains/data/PlayerState.java
@@ -1,49 +1,142 @@
 package com.github.ubiquitousspice.bloodstains.data;
 
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
 import lombok.EqualsAndHashCode;
+import lombok.Getter;
 import lombok.ToString;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.CompressedStreamTools;
+import net.minecraft.nbt.NBTTagCompound;
 
 @EqualsAndHashCode
 @ToString
 public class PlayerState
 {
-	// equipment
-	public final ItemStack currentHeldItem;
-	public final ItemStack[] armour;
+    // equipment
+    @Getter
+    private ItemStack   currentHeldItem;
+    @Getter
+    private ItemStack[] armour;
 
-	// location
-	public final int dimension;
-	public final double x, y, z;
-	public final float yaw, headYaw;
+    // location
+    @Getter
+    private int         dimension;
+    @Getter
+    private double      x, y, z;
+    @Getter
+    private double      motX, motY, motZ;
+    @Getter
+    private float       yaw, headYaw;
 
-	// health and well bieng
-	public final int food;
-	public final float health;
+    @Getter
+    private boolean     isBurning, isOnGround, isSwimming;
 
-	// TODO: account for drowning, fire death, hit status, and flying/falling
+    public PlayerState(EntityPlayer player)
+    {
+        // equipment
+        currentHeldItem = player.getHeldItem() == null ? null : player.getHeldItem().copy();
+        armour = new ItemStack[player.inventory.armorInventory.length];
+        for (int i = 0; i < armour.length; i++)
+        {
+            armour[i] = player.inventory.armorInventory[i] == null ? null : player.inventory.armorInventory[i].copy();
+        }
 
-	public PlayerState(EntityPlayer player)
-	{
-		// equoment
-		currentHeldItem = player.getHeldItem() == null ? null : player.getHeldItem().copy();
-		armour = new ItemStack[player.inventory.armorInventory.length];
-		for (int i = 0; i < armour.length; i++)
-		{
-			armour[i] = player.inventory.armorInventory[i] == null ? null : player.inventory.armorInventory[i].copy();
-		}
+        // location
+        dimension = player.worldObj.provider.dimensionId;
+        x = player.posX;
+        y = player.posY;
+        z = player.posZ;
+        motX = player.motionX;
+        motY = player.motionY;
+        motZ = player.motionZ;
+        yaw = player.rotationYaw;
+        headYaw = player.rotationYawHead;
 
-		// location
-		dimension = player.worldObj.provider.dimensionId;
-		x = player.posX;
-		y = player.posY;
-		z = player.posZ;
-		yaw = player.rotationYaw;
-		headYaw = player.rotationYawHead;
+        // burning and drowning
+        isBurning = player.isBurning();
+        isOnGround = player.onGround;
+        isSwimming = player.isInWater();
+    }
 
-		// food and stuff
-		food = player.getFoodStats().getFoodLevel();
-		health = player.getHealth();
-	}
+    private PlayerState() {}
+
+    public void writeTo(DataOutput output) throws IOException
+    {
+        // inventory
+        writeItem(output, currentHeldItem);
+        output.writeByte(armour.length);
+        for (ItemStack stack : armour)
+            writeItem(output, stack);
+
+        // fields in order
+        output.writeInt(dimension);
+        output.writeDouble(x);
+        output.writeDouble(y);
+        output.writeDouble(z);
+        output.writeDouble(motX);
+        output.writeDouble(motY);
+        output.writeDouble(motZ);
+        output.writeFloat(yaw);
+        output.writeFloat(headYaw);
+        
+        // states
+        output.writeBoolean(isBurning);
+        output.writeBoolean(isOnGround);
+        output.writeBoolean(isSwimming);
+    }
+
+    public static PlayerState readFrom(DataInput input) throws IOException
+    {
+        PlayerState state = new PlayerState();
+
+        // equipment
+        state.currentHeldItem = readItem(input);
+        state.armour = new ItemStack[input.readByte()];
+        for (int i = 0; i < state.armour.length; i++)
+            state.armour[i] = readItem(input);
+
+        // other stuff.
+        state.dimension = input.readInt();
+        state.x = input.readDouble();
+        state.y = input.readDouble();
+        state.z = input.readDouble();
+        state.motX = input.readDouble();
+        state.motY = input.readDouble();
+        state.motZ = input.readDouble();
+        state.yaw = input.readFloat();
+        state.headYaw = input.readFloat();
+        
+        state.isBurning = input.readBoolean();
+        state.isOnGround = input.readBoolean();
+        state.isSwimming = input.readBoolean();
+        
+        return state;
+    }
+
+    private static void writeItem(DataOutput output, ItemStack item) throws IOException
+    {
+        // write a boolean for null status
+        output.writeBoolean(item != null);
+        if (item == null)
+            return; // done
+
+        NBTTagCompound compound = new NBTTagCompound();
+        item.writeToNBT(compound);
+        CompressedStreamTools.write(compound, output);
+    }
+
+    private static ItemStack readItem(DataInput input) throws IOException
+    {
+        // write a boolean for null status
+        if (!input.readBoolean())
+            return null; // done
+
+        NBTTagCompound compound = CompressedStreamTools.read(input);
+
+        return ItemStack.loadItemStackFromNBT(compound);
+    }
 }
