2ecbfdc4327e115ccf8dd20a36ce22a5b287921d
AbrarSyed <sacabrarsyed@gmail.com>
2014-05-18 18:42:03 -0500

    stuff

--- src/main/java/com/github/ubiquitousspice/bloodstains/client/PlaybackPlayer.java
+++ src/main/java/com/github/ubiquitousspice/bloodstains/client/PlaybackPlayer.java
@@ -16,54 +16,53 @@ import com.github.ubiquitousspice.bloodstains.data.BloodStain;
 import com.github.ubiquitousspice.bloodstains.data.PlayerState;
 import com.mojang.authlib.GameProfile;
 
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+
 public class PlaybackPlayer extends EntityOtherPlayerMP
 {
     private Queue<PlayerState> states;
-    
+
     public PlaybackPlayer(BloodStain stain)
     {
-		super(Minecraft.getMinecraft().thePlayer.worldObj, new GameProfile(stain.uid.toString(), stain.username));
-		this.posX = stain.x;
+        super(Minecraft.getMinecraft().thePlayer.worldObj, new GameProfile(stain.uid.toString(), stain.username));
+        this.posX = stain.x;
         this.posY = stain.y;
         this.posZ = stain.z;
-        
-        // convert states to a list of things that can eb safely popped off...
+
+        // convert states to a list of things that can be safely popped off...
         states = new LinkedList<PlayerState>();
         states.addAll(stain.states);
-        
+
         this.capabilities.allowFlying = true;
         this.capabilities.isFlying = true;
         this.capabilities.allowEdit = false;
-        
+        this.capabilities.disableDamage = false;
+
         this.dataWatcher = new DummyDataWatcher(this);
-        
+
         this.setHealth(getMaxHealth());
     }
-    
+
     @Override
     public void onUpdate()
     {
+        super.onUpdate();
+        
         if (states.isEmpty())
         {
             // clean out.
             this.setHealth(0);
-            //this.attackEntityFrom(DamageSource.outOfWorld, 1000f);
-            //this.kill();
-            //this.setDead();
-//            /return;
         }
         else
         {
             PlayerState state = states.poll();
             state.applyTo(this);
         }
-        
-        
-        super.onUpdate();
     }
-    
+
     // proper overrides
-    
+
     @Override
     public void setHealth(float par1)
     {
@@ -76,17 +75,24 @@ public class PlaybackPlayer extends EntityOtherPlayerMP
             super.setHealth(par1);
         }
     }
+    
+    @SideOnly(Side.CLIENT)
+    @Override
+    public boolean getAlwaysRenderNameTagForRender()
+    {
+        return false;
+    }
 
     // stolen from forge fakePlayer + some edits
     @Override protected void entityInit() {}
-    //@Override public boolean attackEntityFrom(DamageSource source, float par2)  { return false;  }
+    @Override public boolean attackEntityFrom(DamageSource source, float par2)  { return false;  }
     @Override public void addChatMessage(IChatComponent chat) {}
     @Override public void addChatComponentMessage(IChatComponent chat){}
     @Override public void addStat(StatBase par1StatBase, int par2){}
     @Override public void openGui(Object mod, int modGuiId, World world, int x, int y, int z){}
-    //@Override public boolean isEntityInvulnerable(){ return true; }
+    @Override public boolean isEntityInvulnerable(){ return true; }
     @Override public boolean canAttackPlayer(EntityPlayer player){ return false; }
-    //@Override public boolean hitByEntity(Entity par1Entity) { return false; }
+    @Override public boolean hitByEntity(Entity par1Entity) { return false; }
     @Override public void onDeath(DamageSource source){ return; }
     @Override public void travelToDimension(int dim){ return; }
     @Override public boolean canCommandSenderUseCommand(int var1, String var2) { return false; }
--- src/main/java/com/github/ubiquitousspice/bloodstains/data/PlayerState.java
+++ src/main/java/com/github/ubiquitousspice/bloodstains/data/PlayerState.java
@@ -1,6 +1,11 @@
 package com.github.ubiquitousspice.bloodstains.data;
 
-import cpw.mods.fml.common.ObfuscationReflectionHelper;
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+import com.github.ubiquitousspice.bloodstains.client.PlaybackPlayer;
+
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
 import net.minecraft.entity.Entity;
@@ -9,10 +14,7 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.CompressedStreamTools;
 import net.minecraft.nbt.NBTTagCompound;
-
-import java.io.DataInput;
-import java.io.DataOutput;
-import java.io.IOException;
+import cpw.mods.fml.common.ObfuscationReflectionHelper;
 
 @EqualsAndHashCode
 @ToString
@@ -26,8 +28,20 @@ public class PlayerState
     private int         dimension;
     private double      x, y, z;
     private double      motX, motY, motZ;
-    private float       yaw, headYaw;
     
+    //
+    boolean sneaking;
+    
+    // rotation
+    private float       yawOffset, yaw, pitch, headYaw, cameraYaw, cameraPitch;
+    
+    // walk
+    private int         stepDist;
+    private float       walkDistMod, walkDistStepMod;
+
+    // swing
+    private int         itemUseCount;
+
     // hit and hurt data
     private int recentlyHit, hurtTime, hurtResistantTime, fireTime;
     private float health;
@@ -45,7 +59,7 @@ public class PlayerState
             armour[i] = player.inventory.armorInventory[i] == null ? null : player.inventory.armorInventory[i].copy();
         }
 
-        // location
+        // location and motion
         dimension = player.worldObj.provider.dimensionId;
         x = player.posX;
         y = player.posY;
@@ -53,8 +67,24 @@ public class PlayerState
         motX = player.motionX;
         motY = player.motionY;
         motZ = player.motionZ;
+        
+        sneaking = player.isSneaking();
+        
+        // rotations
+        yawOffset = player.renderYawOffset;
         yaw = player.rotationYaw;
+        pitch = player.rotationPitch;
         headYaw = player.rotationYawHead;
+        cameraYaw = player.cameraYaw;
+        cameraPitch = player.cameraPitch;
+        
+        // walking
+        stepDist = ObfuscationReflectionHelper.getPrivateValue(Entity.class, player, "nextStepDistance", "field_70150_b");
+        walkDistMod = ObfuscationReflectionHelper.getPrivateValue(Entity.class, player, "distanceWalkedModified", "field_70140_Q");
+        walkDistStepMod = ObfuscationReflectionHelper.getPrivateValue(Entity.class, player, "distanceWalkedOnStepModified", "field_82151_R");
+        
+        // item use
+        itemUseCount = ObfuscationReflectionHelper.getPrivateValue(EntityPlayer.class, player, "itemInUseCount", "field_71072_f");
 
         // burning and drowning
         isOnGround = player.onGround;
@@ -66,19 +96,43 @@ public class PlayerState
 
     private PlayerState() {}
     
-    public void applyTo(EntityPlayer player)
+    public void applyTo(PlaybackPlayer player)
     {
         player.inventory.setInventorySlotContents(player.inventory.currentItem, currentHeldItem);
         player.inventory.armorInventory = armour;
+        
+        player.setPositionAndRotation(x, y, z, yaw, pitch);
+        
+        // location and motion
         player.posX = x;
         player.posY = y;
         player.posZ = z;
         player.motionX = motX;
         player.motionY = motY;
         player.motionZ = motZ;
+        
+        player.setSneaking(sneaking);
 
-        player.rotationYawHead = headYaw;
+        // rotations
+        player.renderYawOffset = yawOffset;
         player.rotationYaw = yaw;
+        player.rotationPitch = pitch;
+        player.rotationYawHead = headYaw;
+        player.cameraYaw = cameraYaw;
+        player.cameraPitch = cameraPitch;
+        
+        // walking
+        ObfuscationReflectionHelper.setPrivateValue(Entity.class, player, stepDist, "nextStepDistance", "field_70150_b");
+        ObfuscationReflectionHelper.setPrivateValue(Entity.class, player, walkDistMod, "distanceWalkedModified", "field_70140_Q");
+        ObfuscationReflectionHelper.setPrivateValue(Entity.class, player, walkDistStepMod,"distanceWalkedOnStepModified", "field_82151_R");
+        
+        // swinging
+        ObfuscationReflectionHelper.setPrivateValue(EntityPlayer.class, player, itemUseCount, "itemInUseCount", "field_71072_f");
+        if (itemUseCount > 0)
+        {
+            //itemInUse
+            ObfuscationReflectionHelper.setPrivateValue(EntityPlayer.class, player, currentHeldItem, "itemInUse", "field_71074_e");
+        }
         
         player.setHealth(health);
         
@@ -102,13 +156,26 @@ public class PlayerState
         output.writeDouble(motX);
         output.writeDouble(motY);
         output.writeDouble(motZ);
+        
+        output.writeBoolean(sneaking);
+        
+        output.writeFloat(yawOffset);
         output.writeFloat(yaw);
+        output.writeFloat(pitch);
         output.writeFloat(headYaw);
+        output.writeFloat(cameraYaw);
+        output.writeFloat(cameraPitch);
         
         // states
         output.writeBoolean(isOnGround);
         output.writeInt(hurtTime);
         output.writeInt(fireTime);
+        
+        output.writeInt(stepDist);
+        output.writeFloat(walkDistMod);
+        output.writeFloat(walkDistStepMod);
+        
+        output.writeInt(itemUseCount);
         
         output.writeFloat(health);
     }
@@ -131,12 +198,25 @@ public class PlayerState
         state.motX = input.readDouble();
         state.motY = input.readDouble();
         state.motZ = input.readDouble();
+        
+        state.sneaking = input.readBoolean();
+        
+        state.yawOffset = input.readFloat();
         state.yaw = input.readFloat();
+        state.pitch =input.readFloat(); 
         state.headYaw = input.readFloat();
+        state.cameraYaw =input.readFloat(); 
+        state.cameraPitch = input.readFloat();
         
         state.isOnGround = input.readBoolean();
         state.hurtTime = input.readInt();
         state.fireTime = input.readInt();
+        
+        state.stepDist = input.readInt();
+        state.walkDistMod = input.readFloat();
+        state.walkDistStepMod = input.readFloat();
+        
+        state.itemUseCount = input.readInt();
         
         state.health = input.readFloat();
         
